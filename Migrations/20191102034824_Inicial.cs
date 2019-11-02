b9255ac12f791355ce36297203bb6bc0060c0542
unknown <Jonathan.3.silva@gmail.com>
2019-11-02 02:20:14 -0300

    Criação da API

---
+++ Migrations/20191102034824_Inicial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    ID_PRODUTO = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DESC_PRODUTO = table.Column<string>(maxLength: 44, nullable: true),
+                    EST_PRODUTO = table.Column<double>(nullable: true),
+                    VAL_PRODUTO = table.Column<double>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.ID_PRODUTO);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int ID_PRODUTO { get; set; }
+        [MaxLength(44)]
+        public string DESC_PRODUTO { get; set; }
+        public double? EST_PRODUTO { get; set; }
+        public double? VAL_PRODUTO { get; set; }
+
+
+    }
+}
